9c964dc72404e15aa5d68bffabfdf36e131523a1
wuby986 <roberto.mariani3@gmail.com>
2015-04-29 14:15:38 +0200

    V Added ondemandplus ans smartmax gov

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 7 */
+/* This file is auto generated, version 4 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#7 SMP PREEMPT Sat Apr 18 01:05:53 CEST 2015"
+#define UTS_VERSION "#4 SMP PREEMPT Wed Apr 29 01:30:10 CEST 2015"
 #define LINUX_COMPILE_BY "wuby"
 #define LINUX_COMPILE_HOST "wuby"
 #define LINUX_COMPILER "gcc version 4.9.x-google 20140827 (prerelease) (SaberMod-4.9.x_android-graphite) "
