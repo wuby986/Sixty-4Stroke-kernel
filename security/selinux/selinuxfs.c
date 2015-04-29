26f9c79a1a195d492dffa578c856d670ee33813f
wuby986 <roberto.mariani3@gmail.com>
2015-04-29 14:22:07 +0200

    selinux full permissive instead of using init.d

--- security/selinux/selinuxfs.c
+++ security/selinux/selinuxfs.c
@@ -168,7 +168,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 		goto out;
 #ifdef CONFIG_ALWAYS_ENFORCE
 	// If build is user build and enforce option is set, selinux is always enforcing
-	new_value = 0;
+	new_value = 1;
 	length = task_has_security(current, SECURITY__SETENFORCE);
 	audit_log(current->audit_context, GFP_KERNEL, AUDIT_MAC_STATUS,
                         "config_always_enforce - true; enforcing=%d old_enforcing=%d auid=%u ses=%u",
@@ -181,6 +181,7 @@ static ssize_t sel_write_enforce(struct file *file, const char __user *buf,
 	selinux_status_update_setenforce(new_value);
 
 #else
+	new_value = 0;
 	if (new_value != selinux_enforcing) {
 		length = task_has_security(current, SECURITY__SETENFORCE);
 		if (length)
